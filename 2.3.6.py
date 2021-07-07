ec13471d66b16410a0bff98781dd46ac65eddbca
Kim Natalia <holly_88_88@mail.ru>
2021-07-07 20:49:09 +0300

    Add files fron the second module

---
+++ 2.3.6.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import time
+import math
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_css_selector("body > form > div > div > button")
+    button.click()
+
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    x_element = browser.find_element_by_css_selector('#input_value')
+    x = int(x_element.text)
+
+
+    def calc(x):
+        return str(math.log(abs(12*math.sin(x))))
+
+    y = calc(x)
+
+    input1 = browser.find_element_by_id('answer')
+    input1.send_keys(y)
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
